bbc0daa301804032b38127ed6d037fba85069c13
Kat Marchán <kzm@sykosomatic.org>
2017-06-28 20:06:21 -0700

    fix(semver): use loose semver parsing for *all* ops

--- test/index.js
+++ test/index.js
@@ -208,3 +208,17 @@ test('matches even if requested version has spaces', t => {
   t.equal(manifest.version, '1.0.0', 'picked the right manifest even though `wanted` had spaced')
   t.done()
 })
+
+test('matches even if requested version has garbage', t => {
+  const metadata = {
+    versions: {
+      '1.0.0': { version: '1.0.0' },
+      '1.0.1': { version: '1.0.1' },
+      '1.0.2': { version: '1.0.2' },
+      '2.0.0': { version: '2.0.0' }
+    }
+  }
+  const manifest = pickManifest(metadata, '== 1.0.0 || foo')
+  t.equal(manifest.version, '1.0.0', 'picked the right manifest even though `wanted` had garbage')
+  t.done()
+})
